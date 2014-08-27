076785d7e81c4318dc6578f4f41d5f136c8db4a0
bergi <bergi@axolotlfarm.org>
2014-08-28 00:07:16 +0200

    code refactoring

--- index.js
+++ index.js
@@ -1,48 +1,69 @@
-var _ = require('underscore'),
-	path = require('path'),
-	url = require('url');
+var
+  path = require('path'),
+  url = require('url');
 
 
-module.exports = function(options) {
-	var absoluteUrl = function() {
-		var absoluteUrl = {
-			'protocol': this.protocol,
-			'hostname': this.host,
-			'port': this.app.settings.port,
-			'pathname': this.url
-		}; 
+module.exports = {};
 
-		if(this.app.get('basePath'))
-			absoluteUrl.pathname = path.join(this.app.get('basePath'), absoluteUrl.pathname);
 
-		if(this.app.get('trust proxy')) {
-			if(!_.isUndefined(this.headers['x-forwarded-proto']))
-				absoluteUrl.protocol = this.headers['x-forwarded-proto'];
+module.exports.absoluteUrl = function (options) {
+  if (options == null) {
+    options = {};
+  }
 
-			if(!_.isUndefined(this.headers['x-forwarded-host']))
-				absoluteUrl.hostname = this.headers['x-forwarded-host'];
+  return function (req, res, next) {
+    req.absoluteUrl = function () {
+      var absoluteUrl = {
+        protocol: this.protocol,
+        hostname: this.host,
+        port: this.socket.address().port,
+        pathname: this.url
+      };
 
-			if(!_.isUndefined(this.headers['x-forwarded-port']))
-				absoluteUrl.port = this.headers['x-forwarded-port'];
-		}
+      if ('basePath' in options) {
+        absoluteUrl.pathname = path.join(options.basePath, absoluteUrl.pathname);
+      }
 
-		if((absoluteUrl.protocol == 'http' && absoluteUrl.port == 80) || (absoluteUrl.protocol == 'https' && absoluteUrl.port == 443))
-			absoluteUrl.port = '';
+      // use proxy header fields?
+      if (req.app.get('trust proxy')) {
+        if ('x-forwarded-proto' in this.headers) {
+          absoluteUrl.protocol = this.headers['x-forwarded-proto'];
+        }
 
-		return url.format(absoluteUrl);
-	};
+        if ('x-forwarded-host' in this.headers) {
+          absoluteUrl.hostname = this.headers['x-forwarded-host'];
+        }
 
-	return function(req, res, next) {
-		if(!_.isUndefined(options.absoluteUrl) && options.absoluteUrl)
-			req.absoluteUrl = absoluteUrl;
+        if ('x-forwarded-port' in this.headers) {
+          absoluteUrl.port = this.headers['x-forwarded-port'];
+        }
+      }
 
-		if(!_.isUndefined(options.defaultSessionParameters)) {
-			for(var parameter in options.defaultSessionParameters) {
-				if(_.isUndefined(req.session[parameter]))
-					req.session[parameter] = options.defaultSessionParameters[parameter];
-			}
-		}
+      // ignore port if default http(s) port
+      if (absoluteUrl.protocol == 'http' && absoluteUrl.port == 80) {
+        absoluteUrl.port = '';
+      }
 
-		next();
-	};
+      if (absoluteUrl.protocol == 'https' && absoluteUrl.port == 443) {
+        absoluteUrl.port = '';
+      }
+
+      return url.format(absoluteUrl);
+    };
+
+    next();
+  };
+};
+
+
+module.exports.defaultSessionParameters = function (parameters) {
+  return function (req, res, next) {
+    for (var parameter in parameters) {
+      if (!(parameter in req.session)) {
+        req.session[parameter] = parameters[parameter];
+      }
+    }
+
+    next();
+  };
 };
